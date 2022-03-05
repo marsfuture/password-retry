9e77eee2b3f4d649c1c63345f7804e5ef242fbef
Eason Wang <ks40113202@gmail.com>
2022-03-06 01:08:38 +0800

    程式更簡潔清楚版

--- password.py
+++ password.py
@@ -1,12 +1,15 @@
 password = 'a123456'
-i = 3
+i = 3 #剩餘機會
 while i > 0:
+    i = i - 1
     pwd = input('請輸入密碼:')
     if pwd == password:
         print('登入成功!')
         break #逃出迴圈
     else:
-        i = i - 1
-        print('密碼錯誤! 還有', i, '次機會') #拆成字串跟整數
-        
+        print('密碼錯誤!')
+        if i > 0:
+            print('還有', i, '次機會') #拆成字串跟整數
+        else:
+            print('已達密碼錯誤上限! 帳戶已被鎖定!')
     
